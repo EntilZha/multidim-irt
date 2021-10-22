29108d20d305f9a62d91216ab1856cbdd368cde0
John P. Lalor <john.lalor@nd.edu>
2021-10-22 10:07:46 -0400

    update plotting code

--- plot_curves_amortized.R
+++ plot_curves_amortized.R
@@ -4,16 +4,14 @@ library(dplyr)
 library(gridExtra)
 library(jsonlite)
 library(stringr)
+library(pROC)
 # file
 
 datasets <- c(
-    "aqa-r1",
-    "bio-asq",
-    "drop_dev",
-    "duo_rc",
-    "news_qa",
-    "race_dev",
-    "relation_extraction"
+    "anli-r1-dev.jsonl.preds.jsonlines",
+    "anli-r2-dev.jsonl.preds.jsonlines",
+    "anli-r3-dev.jsonl.preds.jsonlines",
+    "superglue-winogender.jsonl.preds.jsonlines"
     )
 
 buildPlot  <- function(dataset){
@@ -25,8 +23,8 @@ buildPlot  <- function(dataset){
 
     D.amortized <- stream_in(
         file(
-            str <- glue(
-                "../py-irt/results_mean_field_{dataset}/model_predictions.jsonlines"
+            str_glue(
+                "../py-irt/results/amortized/nli/{dataset}/model_predictions.jsonlines"
             )
         )
     )
@@ -34,8 +32,8 @@ buildPlot  <- function(dataset){
 
     D.1pl <- stream_in(
         file(
-            str <- glue(
-                "../py-irt/results_mean_field_{dataset}/model_predictions.jsonlines"
+            str_glue(
+                "../py-irt/results/meanfield/nli/{dataset}/model_predictions.jsonlines"
             )
         )
     )
@@ -43,13 +41,21 @@ buildPlot  <- function(dataset){
     D.1pl$type  <- "meanfield"
     D.amortized$type  <- "amortized"
 
+    auc.1pl  <- round(auc(D.1pl$response, D.1pl$prediction), 3)
+    auc.amortized  <- round(auc(D.amortized$response, D.amortized$prediction), 3)
+    dname  <- strsplit(dataset, "\\.")[[1]][[1]]
+
+    plot_title  <- str_glue(
+        "{dname} -- Amortized AUC: {auc.amortized}, Meanfield AUC: {auc.1pl}"
+    )
+
     D  <-  D.amortized %>%
         bind_rows(D.1pl)
-    p  <- ggplot(aes(x=difficulty, y=..scaled.., color=type, fill=type), data=D) +
+    p  <- ggplot(aes(x=prediction, y=..scaled.., color=type, fill=type), data=D) +
         geom_density(fill=NA) +
-        ggtitle(dataset)
+        ggtitle(plot_title)
     return(p)
-}
+} 
 
 
 plots  <- lapply(datasets, buildPlot)
