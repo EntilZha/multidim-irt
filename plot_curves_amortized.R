ad3a20bc08887741f545297810e7d617b44dbe42
John P. Lalor <john.lalor@nd.edu>
2021-10-22 15:42:02 -0400

    update plotting code

--- plot_curves_amortized.R
+++ plot_curves_amortized.R
@@ -7,12 +7,25 @@ library(stringr)
 library(pROC)
 # file
 
-datasets <- c(
-    "anli-r1-dev.jsonl.preds.jsonlines",
-    "anli-r2-dev.jsonl.preds.jsonlines",
-    "anli-r3-dev.jsonl.preds.jsonlines",
-    "superglue-winogender.jsonl.preds.jsonlines"
-    )
+data.nli <- c(
+    "nli/anli-r1-dev.jsonl.preds.jsonlines",
+    "nli/anli-r2-dev.jsonl.preds.jsonlines",
+    "nli/anli-r3-dev.jsonl.preds.jsonlines",
+    "nli/superglue-winogender.jsonl.preds.jsonlines"
+)
+
+data.sentiment  <- c(
+    "sentiment/dynasent-r1-dev.jsonl.preds.jsonlines",
+    "sentiment/dynasent-r2-dev.jsonl.preds.jsonlines",
+    "sentiment/sst3-dev.jsonl.preds.jsonlines"
+)
+
+data.hs <- c(
+    "hs/ahs-r1-dev.jsonl.preds.jsonlines",
+    "hs/ahs-r2-dev.jsonl.preds.jsonlines",
+    "hs/ahs-r3-dev.jsonl.preds.jsonlines",
+    "hs/ahs-r4-dev.jsonl.preds.jsonlines"
+)
 
 buildPlot  <- function(dataset){
 #    D.amortized <- jsonlite::read_json(str_glue("../py-irt/results_amortized_{dataset}/best_parameters.json"),
@@ -24,7 +37,7 @@ buildPlot  <- function(dataset){
     D.amortized <- stream_in(
         file(
             str_glue(
-                "../py-irt/results/amortized/nli/{dataset}/model_predictions.jsonlines"
+                "../py-irt/results/amortized/{dataset}/model_predictions.jsonlines"
             )
         )
     )
@@ -33,7 +46,7 @@ buildPlot  <- function(dataset){
     D.1pl <- stream_in(
         file(
             str_glue(
-                "../py-irt/results/meanfield/nli/{dataset}/model_predictions.jsonlines"
+                "../py-irt/results/meanfield/{dataset}/model_predictions.jsonlines"
             )
         )
     )
@@ -44,22 +57,72 @@ buildPlot  <- function(dataset){
     auc.1pl  <- round(auc(D.1pl$response, D.1pl$prediction), 3)
     auc.amortized  <- round(auc(D.amortized$response, D.amortized$prediction), 3)
     dname  <- strsplit(dataset, "\\.")[[1]][[1]]
+    dname  <- strsplit(dname, "\\/")[[1]][[2]]
 
     plot_title  <- str_glue(
-        "{dname} -- Amortized AUC: {auc.amortized}, Meanfield AUC: {auc.1pl}"
+        "{dname}"
+    )
+    plot_text <- str_glue(
+        "Am. AUC: {auc.amortized} MF AUC: {auc.1pl}"
     )
 
     D  <-  D.amortized %>%
         bind_rows(D.1pl)
     p  <- ggplot(aes(x=prediction, y=..scaled.., color=type, fill=type), data=D) +
         geom_density(fill=NA) +
-        ggtitle(plot_title)
+        ggtitle(plot_title) +
+        annotate("text", x=0.5, y=0, label=plot_text)
     return(p)
 } 
 
 
-plots  <- lapply(datasets, buildPlot)
 
-p 
+plots.nli  <- lapply(data.nli, buildPlot)
 
-do.call(grid.arrange, plots)
+p <- do.call(
+    arrangeGrob,
+    plots.nli
+)
+
+ggsave(
+    "nli.png",
+    p,
+    width=10,
+    height=5
+)
+
+
+# sentiment 
+plots.sentiment  <- lapply(data.sentiment, buildPlot)
+
+p <- do.call(
+    arrangeGrob,
+    c(plots.sentiment,nrow=2)
+)
+
+ggsave(
+    "sentiment.png",
+    p,
+    width=8,
+    height=5
+)
+
+
+# HS
+plots.hs  <- lapply(data.hs, buildPlot)
+
+p <- do.call(
+    arrangeGrob,
+    plots.hs
+)
+
+ggsave(
+    "hs.png",
+    p,
+    width=10,
+    height=5
+)
+
+
+
+
